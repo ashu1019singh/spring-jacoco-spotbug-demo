9cad5d54e750c39b2c425f818a17d75de0527027
Ashutosh Singh <ashutosh.9.singh@stackroute.in>
2019-02-04 13:40:44 +0530

    added testcase

--- src/test/java/com/learning/spring/SpringAppTests.java
+++ src/test/java/com/learning/spring/SpringAppTests.java
@@ -1,4 +1,4 @@
-package com.learning.springbootwebsocket;
+package com.learning.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootWebsocketApplicationTests {
+public class SpringAppTests {
 
 	@Test
 	public void contextLoads() {
