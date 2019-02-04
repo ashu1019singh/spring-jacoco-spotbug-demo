cbe58bcf6463664291d49b17153d7ab778dd27d1
Ashutosh Singh <ashutosh.9.singh@stackroute.in>
2019-02-04 14:27:59 +0530

    added one dummy test

---
+++ src/test/java/com/learning/spring/demoTest.java
@@ -0,0 +1,14 @@
+package com.learning.spring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class demoTest {
+
+	@Test
+	public void test() {
+		assertEquals(4, 4);
+	}
+
+}
